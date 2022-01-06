9282dd791990e3db36925a2e825ce81b73d15ca0
Sean-PB <rileydog33@yahoo.com>
2022-01-06 13:16:18 -0800

    GUI button writes run to prng-service.txt

---
+++ CS361/CS 361 Repo/Assignment 2/image_service.py
@@ -0,0 +1,4 @@
+# Reads image-service.txt
+# Erases image-service.txt
+# Uses the prng to select a picture
+# Writes path to picture in image-service.txt
---
+++ CS361/CS 361 Repo/Assignment 2/prng_service.py
@@ -0,0 +1,4 @@
+# Reads prng-service.txt
+# Erases prng-service.txt
+# Makes a pseudo-random number 
+# writes pseudo-random number to prng-service.txt
